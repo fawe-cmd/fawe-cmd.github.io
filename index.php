ac86af419cbb67e8499b6cfe6d928685b5803bcc
fors-cmd <43184641+fors-cmd@users.noreply.github.com>
2018-11-28 20:56:15 +0200

    fawe-cmd.github.io

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <title>Приватный чит для VimeWorld</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="imgaes/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+</head>
+
+<body>
+    <header id="home" class="gradient-violat">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Навигация</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+                    <a class="navbar-brand" href="index.php"><span class="logo-wraper logo-white"><img src="images/Logo.png" alt="Логотип wh-satano"></span></a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav  navbar-right">
+                        <li class="active"><a href="#home">Главная <span class="sr-only">(current)</span></a></li>
+                                      
+                    </ul>
+                </div>
+                <hr class="navbar-divider">
+            </div>
+        </nav>
+    </header>
+    <section id="introduction" class="gradient-violat padding-top-90 home-slider">
+        <div id="home-slider" class="owl-carousel">
+            <div>
+                <div class="sliding-card-with-bottom-image text-center padding-top-90">
+                    <h1 class="cta-heading text-white">Приватный чит для VimeWorld</h1>
+                  <p class="text-white slider-para">При покупке чита навсегда, 1 ключ на месяц идёт в подарок!</p>
+                    <p class="text-white slider-para">RageHack</p>
+                    <div class="cta-btn-group">
+                        <a class="btn btn-orange border-none btn-rounded-corner" href="#buy" rel="nofollow">ПРИОБРЕСТИ</a><br>
+<div class="mac-iframe">
+			<iframe src="https://www.youtube.com/embed/V4ODZ1OAs_Y?ecver=2" width="640" height="360" allowfullscreen=""></iframe> 
+		
+</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section id="buy" class="overflow-x-hidden ceni">
+        <div class="row price_margin">
+            <div class="container"><br>
+                <div class="col-sm-3">
+                    <div class="card prices">
+                        <div class="card-block">
+                            <h4 class="onyx">RageHack</h4>
+                            <p class="price-price"><small class="text-muted blog-category">45 рублей</small></p>
+                            <p class="price-days"><small class="text-muted">1 день</small></p>
+                            <ul>
+                                <li>Чит на VimeWorld</li>
+                                <li><a href="#spoiler-1" data-toggle="collapse" class="button">Функции (нажми)</a>
+                                    <div class="collapse" id="spoiler-1">
+                                        <div class="well">
+                                            <ul>
+                                                <li>ESP</li>
+                                                <li>Fly</li>
+                                                <li>KillAura</li>
+                                                <li>Regen</li>
+                                                <li>AntiBadEffects</li>
+                                                <li>InventoryMove</li>
+                                                <li>BedWarsNuker</li>
+                                                <li>AntiKnockBack</li>
+                                                <li>ChestStealer</li>
+                                                <li>NoSlowDown</li>
+                                                <li>BlockPlace</li>
+                                                <li>AutoArmor</li>
+                                                <li>SpeedMine</li>
+                                                <li>Scaffold</li>
+                                                <li>FastEat</li>
+                                                <li>NoFall</li>
+                                                <li>Step</li>
+                                                <li>ExtraKnockBack</li>
+                                                <li>AutoSoup</li>
+                                                <li>MobAura</li>
+                                                <li>Criticals</li>
+                                                <li>FastBow</li>
+                                                <li>SpeedHack</li>
+                                                <li>StorageEsp</li>
+                                                <li>PlayerEsp</li>
+                                                <li>ItemEsp</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <a class="btn btn-orange border-none btn-rounded-corner button_prices" href="/profile.php">Купить</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card prices">
+                        <div class="card-block">
+                            <h4 class="onyx">RageHack</h4>
+                            <p class="price-price"><small class="text-muted blog-category">100 рублей</small></p>
+                            <p class="price-days"><small class="text-muted">На 7 дней</small></p>
+                            <ul>
+                                <li>Чит на VimeWorld</li>
+                                <li><a href="#spoiler-2" data-toggle="collapse" class="button">Функции (нажми)</a>
+                                    <div class="collapse" id="spoiler-2">
+                                        <div class="well">
+                                            <ul>
+                                                <li>ESP</li>
+                                                <li>Fly</li>
+                                                <li>KillAura</li>
+                                                <li>Regen</li>
+                                                <li>AntiBadEffects</li>
+                                                <li>InventoryMove</li>
+                                                <li>BedWarsNuker</li>
+                                                <li>AntiKnockBack</li>
+                                                <li>ChestStealer</li>
+                                                <li>NoSlowDown</li>
+                                                <li>BlockPlace</li>
+                                                <li>AutoArmor</li>
+                                                <li>SpeedMine</li>
+                                                <li>Scaffold</li>
+                                                <li>FastEat</li>
+                                                <li>NoFall</li>
+                                                <li>Step</li>
+                                                <li>ExtraKnockBack</li>
+                                                <li>AutoSoup</li>
+                                                <li>MobAura</li>
+                                                <li>Criticals</li>
+                                                <li>FastBow</li>
+                                                <li>SpeedHack</li>
+                                                <li>StorageEsp</li>
+                                                <li>PlayerEsp</li>
+                                                <li>ItemEsp</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <a class="btn btn-orange border-none btn-rounded-corner button_prices" href="/profile.php">Купить</a>
+                        </div>
+                    </div>
+                </div><div class="col-sm-3">
+                    <div class="card prices">
+                        <div class="card-block">
+                            <h4 class="onyx">RageHack</h4>
+                            <p class="price-price"><small class="text-muted blog-category">170 рублей</small></p>
+                            <p class="price-days"><small class="text-muted">30 дней</small></p>
+                            <ul>
+                                <li>Чит на VimeWorld</li>
+                                <li><a href="#spoiler-3" data-toggle="collapse" class="button">Функции (нажми)</a>
+                                    <div class="collapse" id="spoiler-3">
+                                        <div class="well">
+                                            <ul>
+                                                <li>ESP</li>
+                                                <li>Fly</li>
+                                                <li>KillAura</li>
+                                                <li>Regen</li>
+                                                <li>AntiBadEffects</li>
+                                                <li>InventoryMove</li>
+                                                <li>BedWarsNuker</li>
+                                                <li>AntiKnockBack</li>
+                                                <li>ChestStealer</li>
+                                                <li>NoSlowDown</li>
+                                                <li>BlockPlace</li>
+                                                <li>AutoArmor</li>
+                                                <li>SpeedMine</li>
+                                                <li>Scaffold</li>
+                                                <li>FastEat</li>
+                                                <li>NoFall</li>
+                                                <li>Step</li>
+                                                <li>ExtraKnockBack</li>
+                                                <li>AutoSoup</li>
+                                                <li>MobAura</li>
+                                                <li>Criticals</li>
+                                                <li>FastBow</li>
+                                                <li>SpeedHack</li>
+                                                <li>StorageEsp</li>
+                                                <li>PlayerEsp</li>
+                                                <li>ItemEsp</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <a class="btn btn-orange border-none btn-rounded-corner button_prices" href="/profile.php">Купить</a>
+                        </div>
+                    </div>
+                </div><div class="col-sm-3">
+                    <div class="card prices">
+                        <div class="card-block">
+                            <h4 class="onyx">RageHack</h4>
+                            <p class="price-price"><small class="text-muted blog-category">300 рублей</small></p>
+                            <p class="price-days"><small class="text-muted">Навсегда</small></p>
+                            <ul>
+                                <li>Чит на VimeWorld</li>
+                                <li>АКЦИЯ - ЕСЛИ ВЫ КУПИТЕ ЧИТ НАВСЕГДА, ТО ПОЛУЧИТЕ 1 КЛЮЧ НА МЕСЯЦ В ПОДАРОК!</li>
+                                <li><a href="#spoiler-4" data-toggle="collapse" class="button">Функции (нажми)</a>
+                                    <div class="collapse" id="spoiler-4">
+                                        <div class="well">
+                                            <ul>
+                                                <li>ESP</li>
+                                                <li>Fly</li>
+                                                <li>KillAura</li>
+                                                <li>Regen</li>
+                                                <li>AntiBadEffects</li>
+                                                <li>InventoryMove</li>
+                                                <li>BedWarsNuker</li>
+                                                <li>AntiKnockBack</li>
+                                                <li>ChestStealer</li>
+                                                <li>NoSlowDown</li>
+                                                <li>BlockPlace</li>
+                                                <li>AutoArmor</li>
+                                                <li>SpeedMine</li>
+                                                <li>Scaffold</li>
+                                                <li>FastEat</li>
+                                                <li>NoFall</li>
+                                                <li>Step</li>
+                                                <li>ExtraKnockBack</li>
+                                                <li>AutoSoup</li>
+                                                <li>MobAura</li>
+                                                <li>Criticals</li>
+                                                <li>FastBow</li>
+                                                <li>SpeedHack</li>
+                                                <li>StorageEsp</li>
+                                                <li>PlayerEsp</li>
+                                                <li>ItemEsp</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <a class="btn btn-orange border-none btn-rounded-corner button_prices" href="/profile.php">Купить</a>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+       </div>
+        </section>
+    <section id="sellers" class="padding-top-bottom-120 newsletter">
+        <div class="container">
+            <div class="row">
+                <div class="subscr  iption-wraper text-center">
+                    <h4>Наш персонал</h4>
+                    <p>У этих людей Вы можете получить ответы на Ваши вопросы.</p>
+                    <div id="teammembers">
+    <br>
+    <div class="member hideme"><img src="/images/avatars/ava3.png" class="memberphoto">
+        <h3 class="membertitle">Администратор</h3>
+        <p>Должность: Кодер, Разработчик!</p>
+        <div class="teamsocial"><a style="font-family: Roboto, sans-serif;" target="_blank" href="https://vk.com/id518342292">Связаться</a></div><br>
+    </div>
+	<div class="member hideme"><img src="/images/avatars/ava3.png" class="memberphoto">
+        <h3 class="membertitle">Администратор</h3>
+        <p>Должность: Кодер, Разработчик!</p>
+        <div class="teamsocial"><a style="font-family: Roboto, sans-serif;" target="_blank" href="https://vk.com/id518342292">Связаться</a></div><br>
+    </div>
+    </div>
+</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <center>
+         <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4078704,4,10,200,40,00011111']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+_Hasync.push(['Histats.framed_page', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4078704&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+    </center>
+            <div class="copyright-cta"><center>
+                <hr width="75%">
+                <p>Все права защищены. Любое копирование материалов на нашем сайте карается блокировкой в поисковых системах.</center></p><hr width="75%">
+            </div>
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
